36af4c403c5a3a0d5e138f49afa6951c059acc0e
tarun ramireddy <tarunramireddy@gmail.com>
2025-02-01 12:34:05 -0500

    added resume

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
             <a href="#experience" className="nav-link"><span className="nav-number">02.</span>Experience</a>
             <a href="#work" className="nav-link"><span className="nav-number">03.</span>Work</a>
             <a href="#contact" className="nav-link"><span className="nav-number">04.</span>Contact</a>
-            <a href="/newProfle/ResumeTarunRamireddy.pdf" className="button-outline" download>
-              Resume <FileText className="w-4 h-4" />
+            <a href="/newProfle/ResumeTarunRamireddy-converted.html" className="button-outline" target='_blank'>
+              Resume
             </a>
 
           </div>
